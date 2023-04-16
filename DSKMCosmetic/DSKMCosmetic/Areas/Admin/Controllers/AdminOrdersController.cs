f1b1c02fe75a7ec62af7734a1720fa7e081a2f76
s-white-owl <cookiezi2507@gmail.com>
2023-04-16 13:42:57 +0700

    Added Order Management

---
+++ DSKMCosmetic/DSKMCosmetic/Areas/Admin/Controllers/AdminOrdersController.cs
@@ -0,0 +1,29 @@
+﻿using DSKMCosmetic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DSKMCosmetic.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminOrdersController : Controller
+    {
+        private readonly dksm_cosmeticContext _context;
+
+        public AdminOrdersController(dksm_cosmeticContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Order> orderList = _context.Orders
+                                    .AsNoTracking()
+                                    .Where(o => o.ShippingAddress != null)
+                                    .OrderByDescending(o => o.OrderDate)
+                                    .ToList();
+            ViewBag.Orders = orderList;
+            return View(orderList);
+        }
+    }
+}
